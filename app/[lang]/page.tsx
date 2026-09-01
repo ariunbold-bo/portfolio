21fbe2689ca430f54f123788d10cf8bf304abd94
Hu Tao <ariunboldbold200@gmail.com>
2026-09-02 00:28:57 +0800

    chore ig kinda sleepy changed few things retiring my typewriter optimized a video that was over 40mb in size now its idk 20? or smt

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -19,7 +19,7 @@ export default async function LandingZone(props: {
     <main className="relative z-10 mx-auto flex min-h-screen max-w-5xl flex-col px-4 pt-10 pb-28 sm:px-6 sm:pt-12 md:px-12 md:pt-24 lg:pl-32 lg:pr-12">
       <div className="flex w-full flex-col space-y-28 sm:space-y-36 md:space-y-56">
         <Hero dict={dict} lang={lang} />
-        <About dict={dict} lang={lang} />
+        <About dict={dict} />
         <Projects dict={dict} />
         <Hardware dict={dict} lang={lang} />
         {/* due to being useless ill be moving it to idk doodle? im gonna import it somewhere*/}
